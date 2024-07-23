4a78f9f2005fe451d47bb2921180ba5182de244d
jingyima <jinma717@gmail.com>
2024-07-22 22:20:50 -0400

    Added the playround function

--- javascript.js
+++ javascript.js
@@ -9,8 +9,6 @@ function getComputerChoice() {
     }    
 }
 
-console.log(getComputerChoice())
-
 /* get the human choice */
 
 choice = prompt("Choose rock, paper or scissor")
@@ -27,10 +25,42 @@ function getHumanChoice() {
     }
 }
 
-console.log(getHumanChoice());
-
 /* declare the players score variables */ 
 
 let humanScore = 0;
 let computerScore = 0;
 
+/* write the logic to play a single roud */
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === "rock" && computerChoice === "scissor"){
+        humanScore++;
+        return ["You win!", humanScore, "vs", computerScore];
+    } else if (humanChoice === computerChoice) {
+        return ["It's a tie!", humanScore, "vs", computerScore]
+    } else if (humanChoice === "scissor" && computerChoice === "rock"){
+        computerScore++;
+        return ["You lost!", humanScore, "vs", computerScore];
+    } else if (humanChoice === "paper" && computerChoice === "scissor"){
+        computerScore++;
+        return ["You lost!", humanScore, "vs", computerScore];
+    } else if (humanChoice === "scissor" && computerChoice === "paper"){
+        humanScore++;
+        return ["You win!", humanScore, "vs", computerScore];
+    } else if (humanChoice === "paper" && computerChoice === "rock"){
+        humanScore++;
+        return ["You win!", humanScore, "vs", computerScore];
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        return ["You lost!", humanScore, "vs", computerScore];
+    }
+}
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log (humanSelection)
+console.log (computerSelection)
